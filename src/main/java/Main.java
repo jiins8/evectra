53560d5e171cd368d7c176f54b4a75264507176f
jiins8 <117478765+jiins8@users.noreply.github.com>
2024-02-20 10:22:49 +0100

    se guardan todos los contactos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,24 @@
 // ... (importaciones y otras clases)
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.awt.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.sql.*;
 import java.util.Collections;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         realizarGET();
     }
 
@@ -22,75 +27,131 @@ public class Main {
         Connection connection = null;
 
         try {
-            // URL de conexión para SQLite con la ruta completa al archivo en el escritorio
             String url = "jdbc:sqlite:/C:\\Users\\jinsh\\Desktop\\prueba.db";
-
-            // Conexión a la base de datos
             connection = DriverManager.getConnection(url);
-
-            // Imprime un mensaje cuando la conexión se ha establecido con éxito
-            System.out.println("Conexión establecida con éxito.");
-
-            // Realiza operaciones en la base de datos...
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return connection; // Devuelve la conexión
+        return connection;
     }
 
+    /*
+        public static void realizarGET() {
+            try {
+                // URL de la API del CRM para obtener la lista de contactos
+                String apiUrl = "https://api.clientify.net/v1/contacts/";
 
-    public static void realizarGET() {
-        try {
-            // URL de la API del CRM para obtener la lista de contactos
-            String apiUrl = "https://api.clientify.net/v1/contacts/";
+                // Crea una URL y establece la conexión
+                URL url = new URL(apiUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            // Crea una URL y establece la conexión
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // Configura el método de la solicitud (GET)
+                connection.setRequestMethod("GET");
 
-            // Configura el método de la solicitud (GET)
-            connection.setRequestMethod("GET");
+                // Establece las cabeceras de la solicitud si es necesario (por ejemplo, autenticación)
+                connection.setRequestProperty("Authorization", "Token c5b6325daa345e3c6d50c57bed7d52f3633a198b");
 
-            // Establece las cabeceras de la solicitud si es necesario (por ejemplo, autenticación)
-            connection.setRequestProperty("Authorization", "Token c5b6325daa345e3c6d50c57bed7d52f3633a198b");
+                // Obtiene la respuesta del servidor
+                int responseCode = connection.getResponseCode();
 
-            // Obtiene la respuesta del servidor
-            int responseCode = connection.getResponseCode();
+                // Lee la respuesta del servidor
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
 
-            // Lee la respuesta del servidor
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
 
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
+                    in.close();
+
+                    // Formatea la respuesta JSON
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    JsonNode jsonNode = objectMapper.readTree(response.toString());
+                    String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+
+                    // Imprime la respuesta formateada
+                    System.out.println("Respuesta del servidor:\n" + prettyJson);
+                    insertarContactosEnDB(jsonNode, connectionSQLite());
+                    connection.disconnect();
+                } else {
+                    System.out.println("Error al realizar la solicitud. Código de respuesta: " + responseCode);
                 }
 
-                in.close();
-
-                // Formatea la respuesta JSON
-                ObjectMapper objectMapper = new ObjectMapper();
-                JsonNode jsonNode = objectMapper.readTree(response.toString());
-                String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
-
-                // Imprime la respuesta formateada
-                System.out.println("Respuesta del servidor:\n" + prettyJson);
-                insertarContactosEnDB(jsonNode, connectionSQLite());
+                // Cierra la conexión
                 connection.disconnect();
-            } else {
-                System.out.println("Error al realizar la solicitud. Código de respuesta: " + responseCode);
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+        }
 
-            // Cierra la conexión
-            connection.disconnect();
+     */
+    public static void realizarGET() throws MalformedURLException {
+        try {
+            String apiUrl = "https://api.clientify.net/v1/contacts/";
 
-        } catch (Exception e) {
+            while (apiUrl != null) {
+                // Crea una URL y establece la conexión
+                URL url = new URL(apiUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Authorization", "Token c5b6325daa345e3c6d50c57bed7d52f3633a198b");
+
+                // Obtiene la respuesta del servidor
+                int responseCode = connection.getResponseCode();
+
+                // Lee la respuesta del servidor
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String inputLine;
+                    StringBuilder response = new StringBuilder();
+
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+
+                    in.close();
+
+                    // Formatea la respuesta JSON
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    JsonNode jsonNode = objectMapper.readTree(response.toString());
+                    String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+
+                    //System.out.println("Respuesta del servidor:\n" + prettyJson);
+
+
+                    insertarContactosEnDB(jsonNode, connectionSQLite());
+
+                    // Obtiene la URL de la siguiente página
+                    JsonNode nextNode = jsonNode.get("next");
+                    apiUrl = (nextNode != null) ? nextNode.asText() : null;
+
+                    // Cierra la conexión
+                    connection.disconnect();
+                } else {
+                    System.out.println("Error al realizar la solicitud. Código de respuesta: " + responseCode);
+                    break;
+                } System.out.println("Contactos insertados en la base de datos correctamente.");
+
+            }
+        } catch (ProtocolException e) {
             e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al abrir la conexión.");
         }
     }
+
+
 
     public static void insertarContactosEnDB(JsonNode jsonNode, Connection connection) {
         try {
@@ -148,10 +209,12 @@ public class Main {
                     address = (addressNode != null) ? addressNode.asText() : "";
                 }
 
-                // Prepara la consulta SQL para insertar el contacto en la base de datos
-                String sql = "INSERT OR REPLACE INTO contactos (id, first_name, last_name, title, company_name, phone, email, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                JsonNode createdNode = contactoNode.get("created");
+                String created = (createdNode != null) ? createdNode.asText() : "";
+
+                String sql = "INSERT OR REPLACE INTO contactos (id, first_name, last_name, title, company_name, phone, email, country, created) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
                 try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                    preparedStatement.setInt(1,id);
+                    preparedStatement.setInt(1, id);
                     preparedStatement.setString(2, nombre);
                     preparedStatement.setString(3, apellido);
                     preparedStatement.setString(4, title);
@@ -159,17 +222,14 @@ public class Main {
                     preparedStatement.setString(6, phoneNumber);
                     preparedStatement.setString(7, email);
                     preparedStatement.setString(8, address);
+                    preparedStatement.setString(9, created);
 
-                    // ... establece más parámetros según la estructura de tu base de datos y JSON
-
-                    // Ejecuta la consulta
                     preparedStatement.executeUpdate();
                 } catch (SQLException e) {
                     e.printStackTrace();
                     System.out.println("Error al insertar el contacto en la base de datos.");
                 }
             }
-            System.out.println("Contactos insertados en la base de datos correctamente.");
 
         } catch (Exception e) {
             e.printStackTrace();
